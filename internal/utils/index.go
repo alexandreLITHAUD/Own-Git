19b1b3a4310a248d8b993feed98cc918b3038d36
Alexandre Lithaud <alexandre.ltd71@gmail.com>
2025-04-12 10:23:32 +0200

    Add object test + doc

--- internal/utils/index.go
+++ internal/utils/index.go
@@ -13,10 +13,20 @@ import (
 // TODO Add a function to remove an entry from the index without rewriting the whole index
 // TODO Boyer–Moore string-search algorithm ???
 
+// GetIndexFilePath returns the path of the index file. This could be improve to check
+// if the .own-git folder exists in parents.
+//
+// The path is the path of the current working directory plus the name of the
+// .own-git folder plus the name of the index file.
 func GetIndexFilePath() string {
 	return filepath.Join(".", ".own-git", "index")
 }
 
+// IsIndex checks if the index exists.
+//
+// The function first checks if the .own-git folder exists. If it does, it then
+// checks if the index file exists. If it does, the function returns true;
+// otherwise, it returns false.
 func IsIndex() bool {
 	if !IsOwnFolder() {
 		return false
@@ -29,6 +39,12 @@ func IsIndex() bool {
 	return true
 }
 
+// IsIndexEmpty checks if the index is empty.
+//
+// The function first checks if the index exists. If it does not, the function
+// returns true and a nil error. If the index does exist, the function then
+// checks if the file size is 0. If it is, the function returns true and a nil
+// error; otherwise, the function returns false and a nil error.
 func IsIndexEmpty() (bool, error) {
 	if !IsIndex() {
 		return true, nil
@@ -46,6 +62,14 @@ func IsIndexEmpty() (bool, error) {
 	return false, nil
 }
 
+// ParseIndex reads the index file and parses it into a slice of IndexEntry.
+//
+// If the index does not exist, the function returns nil and an
+// error. If the index exists but is empty, the function returns an empty slice
+// and a nil error. If the index exists and is not empty, the function reads the
+// file, unmarshals the JSON data into a slice of IndexEntry, and returns the
+// slice and a nil error. If an error occurs while reading the file or
+// unmarshalling the JSON data, the function returns nil and the error.
 func ParseIndex() ([]types.IndexEntry, error) {
 	isempty, err := IsIndexEmpty()
 	if err != nil {
@@ -71,6 +95,12 @@ func ParseIndex() ([]types.IndexEntry, error) {
 	return entries, nil
 }
 
+// MergeIndexEntries merges two slices of IndexEntry.
+//
+// This function takes two slices of IndexEntry: the current index and new entries.
+// It creates a map of the index entries using their paths as keys. New entries
+// override existing entries with the same path. The function returns a slice of
+// merged index entries, effectively combining the current index with the new entries.
 func MergeIndexEntries(currentIndex []types.IndexEntry, newEntries []types.IndexEntry) []types.IndexEntry {
 	// TODO Add a function to remove an entry from the index without rewriting the whole index
 
@@ -90,6 +120,11 @@ func MergeIndexEntries(currentIndex []types.IndexEntry, newEntries []types.Index
 	return merged
 }
 
+// WriteEntryToIndex writes a slice of IndexEntry to the index.
+//
+// This function takes a slice of IndexEntry and writes it to the index file.
+// It first parses the current index, merges the new entries with the current index,
+// and then writes the merged index to disk.
 func WriteEntryToIndex(entries []types.IndexEntry) error {
 
 	currentIndex, err := ParseIndex()
@@ -113,6 +148,13 @@ func WriteEntryToIndex(entries []types.IndexEntry) error {
 	return nil
 }
 
+// RemoveEntryFromIndex removes an entry from the index by its file path.
+//
+// The function first parses the current index to get a list of IndexEntry.
+// It then iterates over the entries and removes the entry that matches the
+// provided path. After the entry is removed, it marshals the updated list
+// back to JSON and writes it to the index file. If an error occurs at any
+// point, the function returns the error.
 func RemoveEntryFromIndex(path string) error {
 	currentIndex, err := ParseIndex()
 	if err != nil {
@@ -141,11 +183,18 @@ func RemoveEntryFromIndex(path string) error {
 	return nil
 }
 
+// FilePathtoIndexEntry takes a file path and returns an IndexEntry.
+//
+// It first determines the file mode, either 100644 or 100755 depending on
+// if the file is executable. It then computes the SHA1 of the file and
+// creates an IndexEntry with the file path, mode, and hash. If an error
+// occurs while computing the SHA1, the function returns an error.
 func FilePathtoIndexEntry(path string) (types.IndexEntry, error) {
 
 	var mode string = "100644"
 
 	isexec, err := IsExecutable(path)
+	// CHECK ERROR ??
 	if isexec && err == nil {
 		mode = "100755"
 	}
--- internal/utils/indexstatus.go
+++ internal/utils/indexstatus.go
@@ -1,8 +1,101 @@
 package utils
 
-const (
-	added    uint8 = iota // IF IN INDEX BUT NOT IN OBJECT
-	removed               // IF NOT IN INDEX BUT IN OBJECT
-	modified              // IF IN INDEX AND IN OBJECT WITH SAME NAME
-	renamed               // IF IN INDEX AND IN OBJECT BUT WITH DIFFERENT NAME
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"own/internal/types"
+	"path/filepath"
 )
+
+const (
+	added      uint8 = iota // IF IN INDEX BUT NOT IN OBJECT
+	removed                 // IF NOT IN INDEX BUT IN OBJECT
+	modified                // IF IN INDEX AND IN OBJECT WITH SAME NAME
+	renamed                 // IF IN INDEX AND IN OBJECT BUT WITH DIFFERENT NAME
+	untracked               // IF NOT IN INDEX AND NOT IN OBJECT
+	ignored                 // IF NOT IN INDEX AND NOT IN OBJECT AND IGNORED
+	conflicted              // IF IN INDEX AND IN OBJECT BUT WITH DIFFERENT CONTENT
+	unknown                 // ERROR CASE
+)
+
+func GetFileStatusString(status uint8) string {
+	switch status {
+	case added:
+		return "added"
+	case removed:
+		return "removed"
+	case modified:
+		return "modified"
+	case renamed:
+		return "renamed"
+	case untracked:
+		return "untracked"
+	case ignored:
+		return "ignored"
+	case conflicted:
+		return "conflicted"
+	default:
+		return "unknown"
+	}
+}
+
+func GetObjectFile(hash string) (types.WorktreeEntry, error) {
+
+	path := filepath.Join(".", ".own-git", "objects", hash[:2], hash[2:])
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return types.WorktreeEntry{}, err
+	}
+
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return types.WorktreeEntry{}, err
+	}
+
+	var fileEntry types.WorktreeEntry
+	err = json.Unmarshal(file, &fileEntry)
+	if err != nil {
+		return types.WorktreeEntry{}, err
+	}
+
+	return fileEntry, nil
+}
+
+// GetFileStatus determines the status of a file in the index and object store.
+//
+// If the index does not exist, the function returns unknown and an error.
+// The function then parses the index, creates an IndexEntry for the file using
+// FilePathtoIndexEntry, and then compares the hash of the file entry with the
+// hash of the object file in the object store using GetObjectFile.
+//
+// The function returns the status of the file as a uint8 based on the following
+// cases:
+//
+// - added:      IF IN INDEX BUT NOT IN OBJECT
+// - removed:    IF NOT IN INDEX BUT IN OBJECT
+// - modified:   IF IN INDEX AND IN OBJECT WITH SAME NAME
+// - renamed:    IF IN INDEX AND IN OBJECT BUT WITH DIFFERENT NAME
+// - untracked:  IF NOT IN INDEX AND NOT IN OBJECT
+// - ignored:    IF NOT IN INDEX AND NOT IN OBJECT AND IGNORED
+// - conflicted: IF IN INDEX AND IN OBJECT BUT WITH DIFFERENT CONTENT
+// - unknown:    ERROR CASE
+func GetFileStatus(path string) (uint8, error) {
+
+	if !IsIndex() {
+		return unknown, fmt.Errorf("index does not exist")
+	}
+
+	currentIndexEntries, err := ParseIndex()
+	if err != nil {
+		return unknown, err
+	}
+
+	fileEntry, err := FilePathtoIndexEntry(path)
+	if err != nil {
+		return unknown, err
+	}
+
+	objectFile := GetObjectFile(fileEntry.Hash)
+
+	return unknown, nil
+}
